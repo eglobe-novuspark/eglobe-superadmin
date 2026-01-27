0690fe36078a11b741b3cd5796e830140aee896a
Sadique Shafi <edglobenovuspark@gmail.com>
2026-01-27 20:09:02 +0530

    vercel

--- frontend/src/environments/environments.prod.ts
+++ frontend/src/environments/environments.prod.ts
@@ -3,12 +3,12 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://edglobe.vercel.app',
+  apiUrl: 'https://eglobe-superadmin.vercel.app',
   razorpayKey: 'rzp_live_xxxxxxxxxxxx',
   razorpayKeyId:'',
   googleMapsApiKey:'AIzaSyDKsK100m1obqgkQJILOEafWm2mReD0whA',
   testMode: false,
-  imageUrl:'https://edglobe.vercel.app',
+  imageUrl:'https://eglobe-superadmin.vercel.app',
   masterKey: '269b5f601ec89cc265a0150bc5826459fb0f90f38914dd22f221dc868c1ef3bd',
   deviceFp: '8e61a7e7ecce44b9258259799eeb74ee26da247c351af19bc7057644062759f0',
 };
